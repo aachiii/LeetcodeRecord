bb6867e158e4787e764376271b4c63b9cce3f1fc
Chiqu Li <49333449+aachiii@users.noreply.github.com>
2020-08-02 18:02:45 -0400

    Update 207. Course Schedule.py

--- Graph/207. Course Schedule.py
+++ Graph/207. Course Schedule.py
@@ -1,4 +1,5 @@
 '''
+https://leetcode.com/problems/course-schedule/
 There are a total of numCourses courses you have to take, labeled from 0 to numCourses-1.
 
 Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
@@ -43,5 +44,16 @@ class Solution:
                 return False
         return True
             
-        
-        
+'''        
+Explaination:
+
+Graph is represented in a edge list way.
+Used visited[] to mark visited node in one searching loop, in order to avoid meet same node in one searching loop.
+'''
+
+'''
+首先把每门课的prerequisite都放进一个对应index的list里
+对于每一次调用dfs函数，里面有两个visited[i]的赋值过程。第一个赋值为-1，如果遇见了-1，那么证明在这个dfs大循环里遇到了之前要上的课，return False
+如果没有遇到，则为1，在之后的dfs里，遇到1的证明都行得通
+'''
+
